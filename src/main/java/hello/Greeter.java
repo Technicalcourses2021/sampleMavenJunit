368ffe415dd681f1e5169c15904f1596703f9e49
Technicalcourses2021 <85402649+Technicalcourses2021@users.noreply.github.com>
2021-09-15 12:29:00 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,7 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- Sudheer!! Hi or Bye tca is the string needed here";
+    return "Hello world----- Sudheer!! Hi or Bye is the string needed here ye he";
   }
 }
+
